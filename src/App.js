5f2e009a0c413d88c4aac44a3fcf3b51addb1804
Diogo Goncalves <diogo@ticketmates.com.au>
2020-09-09 22:55:08 +1000

    Filter projects

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Logo from './components/Logo.js'
 import Loading from './components/Loading.js'
 import Feedback from './components/Feedback.js'
 import Projects from './components/Projects.js'
+import Filter from './components/Filter.js'
 import axios from 'axios'
 
 class App extends React.Component {
     state = {
         loading: false,
         isFeedbackVisible: false,
-        projects: []
+        isFilterVisible: false,
+        isProjectsVisible: false,
+        projects: [],
+        states: [],
+        status: [],
+        eta: [],
+        communication: []
     }
 
     componentDidMount = () => {
@@ -26,12 +33,60 @@ class App extends React.Component {
                     password: process.env.REACT_APP_API_PWD
                 }
             })
-            this.setState({projects: data.projects, loading: false}, () => {
+            console.log('data', data)
+
+            this.setState({
+                projects: data.projects, 
+                loading: false,
+                isFilterVisible: true,
+                isProjectsVisible: true,
+                states: data.form.inputs.find(i => i.label === 'State').options,
+                status: data.form.inputs.find(i => i.label === 'Status').options,
+                eta: data.form.inputs.find(i => i.label === 'ETA Report').options,
+                communication: data.form.inputs.find(i => i.label === 'Communication').options
+                }, () => {
                 console.log('projects', this.state.projects)
             })
         } catch (e) {
             console.log('Error fetching projects', e)
-            this.setState({loading: false, isFeedbackVisible: true})
+            this.setState({
+                loading: false, 
+                isFeedbackVisible: true
+            })
+        }
+    }
+
+    onFilter = async (form) => {
+        console.log('form', form)
+        const queryParams = Object.keys(form).map((key) => {
+            return `${encodeURIComponent(key)}=${encodeURIComponent(form[key])}`
+        }).join('&')
+        console.log('queryParams', queryParams)
+
+        this.setState({loading: true, isProjectsVisible: false})
+        try {
+            const {data} = await axios.get(`${process.env.REACT_APP_API_URL}?${queryParams}`, {
+                auth: {
+                    username: process.env.REACT_APP_API_USERNAME,
+                    password: process.env.REACT_APP_API_PWD
+                }
+            })
+            console.log('data', data)
+
+            this.setState({
+                projects: data.projects, 
+                loading: false,
+                isProjectsVisible: true
+                }, () => {
+                console.log('projects', this.state.projects)
+            })
+        } catch(e) {
+            console.log('Error filtering projects', e)
+            this.setState({
+                loading: false, 
+                isFeedbackVisible: true,
+                isProjectsVisible: false
+            })
         }
     }
 
@@ -39,8 +94,9 @@ class App extends React.Component {
         return (
             <div className="app">
                 <Logo/>
-                <Loading title="Fetching projects" visible={this.state.loading}/>
+                <Filter visible={this.state.isFilterVisible} states={this.state.states} status={this.state.status} eta={this.state.eta} communication={this.state.communication} onFilter={this.onFilter}/>
                 <Feedback message="Oops something went wrong 🤷🏻‍♀️" visible={this.state.isFeedbackVisible}/>
+                <Loading title="Fetching projects" visible={this.state.loading}/>
                 <Projects projects={this.state.projects}/>
             </div>
         )
---
+++ src/components/Filter.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Formik, Field, Form } from 'formik';
+import './Filter.css'
+
+class Filter extends React.Component {
+
+	state = {isMoreOptionsVisible: false}
+
+	render = () => {
+		const filterClass = this.props.visible ? 'filter filter--active' : 'filter'
+		const moreOptionsFormClass = this.state.isMoreOptionsVisible ? 'filter__form' : 'filter__form filter__form--hidden'
+		return (
+			<Formik 
+				initialValues={{client_full_name: '',
+					project_number: '',
+					address: '',
+					state: '',
+					team_id: '',
+					status: '',
+					eta_status: '',
+					communication: '',
+					stage_title: ''
+				}}
+				onSubmit={values => {
+        			console.log(values)
+        			this.props.onFilter(values)
+      		}}>
+      			<Form className={filterClass}>
+					<h2 className="filter__title">Filter by</h2>
+					<div className="filter__row">
+						<div className="filter__field">
+							<label className="filter__label">Client Name</label>
+							<Field className="filter__input" name="client_full_name" placeholder="e.g. John Smith"/>
+						</div>
+						<div className="filter__field">
+							<label className="filter__label">Project Number</label>
+							<Field className="filter__input" name="project_number" placeholder="e.g. SD99-9999"/>
+						</div>
+						<div className="filter__field">
+							<label className="filter__label">Suburb</label>
+							<Field className="filter__input" name="address" placeholder="e.g. Surfers Paradise"/>
+						</div>
+					</div>
+					<span className="filter__link" onClick={() => this.setState({isMoreOptionsVisible: true})}>More options</span>
+					<div className={moreOptionsFormClass}>
+						<div className="filter__row">
+							<div className="filter__field">
+								<label className="filter__label">State</label>
+								<div className="filter__select-wrap">
+									<Field as="select" className="filter__input" name="state">
+										<option>Select...</option>
+										{this.props.states.map(s => <option key={s.value} value={s.value}>{s.name}</option>)}
+									</Field>
+								</div>
+							</div>
+							<div className="filter__field">
+								<label className="filter__label">Partner</label>
+								<Field className="filter__input" name="team_id[]" placeholder="e.g. ABC Design"/>
+							</div>
+							<div className="filter__field">
+								<label className="filter__label">Staff</label>
+								<Field className="filter__input" name="team_id[]" placeholder="e.g. John Smith"/>
+							</div>
+						</div>
+						<div className="filter__row">
+							<div className="filter__field">
+								<label className="filter__label">Active Status</label>
+								<div className="filter__select-wrap">
+									<Field as="select" className="filter__input" name="status">
+										{this.props.status.map(s => <option key={s.value} value={s.value}>{s.name}</option>)}
+									</Field>
+								</div>
+							</div>
+							<div className="filter__field">
+								<label className="filter__label">ETA</label>
+								<div className="filter__select-wrap">
+									<Field as="select" className="filter__input" name="eta_status">
+										{this.props.eta.map(e => <option key={e.value} value={e.value}>{e.name}</option>)}
+									</Field>
+								</div>
+							</div>
+							<div className="filter__field">
+								<label className="filter__label">Communication Status</label>
+								<div className="filter__select-wrap">
+									<Field as="select" className="filter__input" name="communication">
+										{this.props.communication.map(c => <option key={c.value} value={c.value}>{c.name}</option>)}
+									</Field>
+								</div>
+							</div>
+						</div>
+						<div className="filter__row">
+							<div className="filter__field filter__field--single">
+								<label className="filter__label">Stage Title</label>
+								<Field className="filter__input" name="stage_title" placeholder="e.g. Development Approval"/>
+							</div>
+						</div>
+						<span className="filter__link" onClick={() => this.setState({isMoreOptionsVisible: false})}>Less options</span>
+					</div>
+					<button className="filter__button" type="submit">Filter</button>
+				</Form>
+			</Formik>
+		)
+	}
+}
+
+export default Filter
